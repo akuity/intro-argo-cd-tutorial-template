a0a7aed6eefff9442a6f61ba1f83e9c305792363
Christian Hernandez <christian@chernand.io>
2024-02-29 15:33:19 -0800

    fixing k alias
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,5 +5,6 @@ echo "post-start start" >> ~/status
 # this runs in background each time the container starts
 
 kind export kubeconfig --name scale
+source ~/.bashrc
 
 echo "post-start complete" >> ~/status
