6f55096197f07e8f7b4ab9abad251d4b38077f82
Christian Hernandez <christian@chernand.io>
2024-03-06 16:46:17 -0800

    export the dev kind cluster instead of the scale one
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,9 +4,10 @@ echo "post-start start" >> ~/status.log
 
 # this runs in background each time the container starts
 
-kind export kubeconfig --name scale
-source ~/.bashrc
+kind export kubeconfig --name dev
 
 bash hack/update-repo-for-workshop.sh
+
+source ~/.bashrc
 
 echo "post-start complete" >> ~/status.log
