90d25b08913a3f675665e0f8e65fe6ca388c730e
Nicholas Morey <nicholas@morey.tech>
2024-06-17 22:12:50 +0200

    feat: install argocd on start-up

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e  # Exit on non-zero exit code from commands
 
-echo "post-start start" >> ~/status.log
+echo "$(date): Running post-start.sh" >> ~/.status.log
 
 # this runs in background each time the container starts
 
@@ -9,7 +10,21 @@ k3d kubeconfig merge dev --kubeconfig-merge-default
 
 bash .hack/update-repo-for-workshop.sh
 
-# Best effort env load
-source ~/.bashrc
+kubectl apply -k argocd 2>&1 | tee -a ~/.status.log
 
-echo "post-start complete" >> ~/status.log
+kubectl wait deployment -n argocd --all --for=condition=Available=True --timeout=90s 2>&1 | tee -a ~/.status.log
+ARGOCD_ADMIN_PASSWORD="$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
+echo "${ARGOCD_ADMIN_PASSWORD}" > ~/argo-cd-admin-password.txt
+
+kubectl apply -f apps.yaml 2>&1 | tee -a ~/.status.log
+
+argocd login \
+  "localhost:31443" \
+  --username admin \
+  --password ${ARGOCD_ADMIN_PASSWORD} \
+  --grpc-web \
+  --insecure 2>&1 | tee -a ~/.status.log
+
+echo "Argo CD admin password: ${ARGOCD_ADMIN_PASSWORD}"
+
+echo "$(date): Finished post-start.sh" >> ~/.status.log
