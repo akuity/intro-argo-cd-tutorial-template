b645dab6825053c1760f8d6325e0d3a03c9378d7
Nicholas Morey <nicholas@morey.tech>
2024-07-22 09:29:45 -0400

    fix: use new script

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -7,7 +7,7 @@ echo "post-start start" >> ~/status.log
 # Ensure kubeconfig is set up. 
 k3d kubeconfig merge dev --kubeconfig-merge-default
 
-bash .hack/update-repo-for-workshop.sh
+bash .devcontainer/update-repo-for-workshop.sh
 
 # Best effort env load
 source ~/.bashrc
